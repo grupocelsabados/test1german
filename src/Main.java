bb96e278c98021d7ed5b8321251e0cf554e60873
germanalejandro <raiiezhd78@gmail.com>
2016-03-19 08:06:20 -0500

    Creación del proyecto y de ka clase Main

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author German
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Bienvenido");
+    }
+
+}
